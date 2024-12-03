ecde6da7f4b18ab4dbe4878f582b26690fe85fee
sankOne <alexdevelop@list.ru>
2024-12-04 00:26:49 +0300

    добавлена триангуляция модели и перерасчёт нормалей

--- Simple3DViewer/src/main/java/com/cgvsu/math/Vector3f.java
+++ Simple3DViewer/src/main/java/com/cgvsu/math/Vector3f.java
@@ -1,18 +1,60 @@
 package com.cgvsu.math;
 
-// Это заготовка для собственной библиотеки для работы с линейной алгеброй
 public class Vector3f {
+    public float x, y, z;
+
+    // Конструктор для инициализации координат вектора
     public Vector3f(float x, float y, float z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
+    // Метод для сложения двух векторов
+    public void add(Vector3f other) {
+        this.x += other.x;
+        this.y += other.y;
+        this.z += other.z;
+    }
+
+    // Метод для вычитания векторов
+    public void sub(Vector3f other) {
+        this.x -= other.x;
+        this.y -= other.y;
+        this.z -= other.z;
+    }
+
+    // Векторное произведение двух векторов
+    public void cross(Vector3f v1, Vector3f v2) {
+        this.x = v1.y * v2.z - v1.z * v2.y;
+        this.y = v1.z * v2.x - v1.x * v2.z;
+        this.z = v1.x * v2.y - v1.y * v2.x;
+    }
+
+    // Метод для нормализации вектора (приведение длины к 1)
+    public void normalize() {
+        float length = length();
+        if (length != 0) {
+            this.x /= length;
+            this.y /= length;
+            this.z /= length;
+        }
+    }
+
+    // Метод для вычисления длины вектора
+    public float length() {
+        return (float) Math.sqrt(x * x + y * y + z * z);
+    }
+
+    // Сравнение векторов с точностью до заданного эпсилона
     public boolean equals(Vector3f other) {
-        // todo: желательно, чтобы это была глобальная константа
         final float eps = 1e-7f;
         return Math.abs(x - other.x) < eps && Math.abs(y - other.y) < eps && Math.abs(z - other.z) < eps;
     }
 
-    public float x, y, z;
+    // Метод для получения строкового представления вектора
+    @Override
+    public String toString() {
+        return "Vector3f(" + x + ", " + y + ", " + z + ")";
+    }
 }
--- Simple3DViewer/src/main/java/com/cgvsu/model/Model.java
+++ Simple3DViewer/src/main/java/com/cgvsu/model/Model.java
@@ -1,4 +1,5 @@
 package com.cgvsu.model;
+
 import com.cgvsu.math.Vector2f;
 import com.cgvsu.math.Vector3f;
 
@@ -6,8 +7,59 @@ import java.util.*;
 
 public class Model {
 
+    // Массивы для хранения вершин, текстурных координат, нормалей и полигонов
     public ArrayList<Vector3f> vertices = new ArrayList<Vector3f>();
     public ArrayList<Vector2f> textureVertices = new ArrayList<Vector2f>();
     public ArrayList<Vector3f> normals = new ArrayList<Vector3f>();
     public ArrayList<Polygon> polygons = new ArrayList<Polygon>();
+
+    // Метод для вычисления нормалей модели
+    public void calculateNormals() {
+        // Инициализируем нормали для каждой вершины
+        for (int i = 0; i < vertices.size(); i++) {
+            normals.add(new Vector3f(0, 0, 0)); // Нормали начинаются с нулевых векторов
+        }
+
+        // Для каждой грани модели вычисляем нормаль
+        for (Polygon polygon : polygons) {
+            Vector3f normal = calculatePolygonNormal(polygon);
+
+            // Добавляем нормаль к каждой вершине в полигоне
+            for (int vertexIndex : polygon.getVertexIndices()) {
+                Vector3f vertexNormal = normals.get(vertexIndex);
+                vertexNormal.add(normal);
+                normals.set(vertexIndex, vertexNormal);
+            }
+        }
+
+        // Нормализуем нормали для каждой вершины
+        for (int i = 0; i < normals.size(); i++) {
+            normals.get(i).normalize();
+        }
+    }
+
+    // Метод для вычисления нормали полигона (треугольника)
+    private Vector3f calculatePolygonNormal(Polygon polygon) {
+        // Получаем вершины полигона
+        Vector3f v0 = vertices.get(polygon.getVertexIndices().get(0));
+        Vector3f v1 = vertices.get(polygon.getVertexIndices().get(1));
+        Vector3f v2 = vertices.get(polygon.getVertexIndices().get(2));
+
+        // Вычисление векторов граней
+        Vector3f edge1 = new Vector3f(v1.x - v0.x, v1.y - v0.y, v1.z - v0.z);
+        Vector3f edge2 = new Vector3f(v2.x - v0.x, v2.y - v0.y, v2.z - v0.z);
+
+        // Векторное произведение для нахождения нормали
+        Vector3f normal = new Vector3f(
+                edge1.y * edge2.z - edge1.z * edge2.y,
+                edge1.z * edge2.x - edge1.x * edge2.z,
+                edge1.x * edge2.y - edge1.y * edge2.x
+        );
+
+        // Нормализуем нормаль
+        normal.normalize();
+
+        return normal;
+    }
+
 }
